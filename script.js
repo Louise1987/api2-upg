075b24a3bd25b840803d665b1cf17faa167c0038
Louise1987 <louisethomssonerixon@Louises-MacBook-Pro.local>
2018-11-30 16:33:03 +0100

    ny
    ny

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Startar server
 var express = require('express');
-//var fs = require('fs');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -10,38 +9,34 @@ var argv = require('yargs').argv;
 
 // css mapp
 app.use(express.static('public')); 
-//-----------------------------------------------------------------------------------------
-//Hämtar rhyme.json
-// var fs_data = fs.readFileSync('./file.json');
-// var rhyme = JSON.parse(fs_data);
-//---------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 //Hämtar pug
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: true}));
 
 //-------------------------------------------------------------------------------------------
-
+//Api key
 var apiKey = '66cf27da781d9551f542dd262827e6a8';
 let city = argv.c || 'Stockholm';
 var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 //-----------------------------------------------------------------------------------------------
-//Api key
-app.get('weather', function(request, response){
+//Hämtar form
+app.get('/weather', function(request, response){
 	return response.render('get-form')
 });
-
-//Hämtar värde från api key och skriver ut
-
+//---------------------------------------------------------------------------------------------
+//Hämtar värde från api key och skriver ut 
+app.get('/getsubmit', function(request, response){
 request(url, function (err, response, body){
 	if(err){
 		console.log('error:', error);
 	} else {
 		var weather = JSON.parse(body)
-		var message = `Det ${weather.main.temp} grader i ${weather.name}!`;
+		var message = `Det är ${weather.main.temp} grader i ${weather.name}!`;
 		console.log(message);		
-		response.send("visar väder");
-		//console.log(body);
+		response.send(message);
 	}
+});
 });
 //-----------------------------------------------------------------------------------------
 
